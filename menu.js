f7efc212349b005fcf600495756a6537ccce7122
Rahul150811 <fnu.rahul25@student.qcc.cuny.edu>
2023-03-12 20:55:39 -0500

    Initial Commit

--- menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    var links = document.querySelector(".links");
-    if (links.style.display === "block") {
-        links.style.display = "none";
-    } else {
-        links.style.display = "block";
-    }
-}
